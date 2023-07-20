32a8ff2f2f3fead8d9838d0c3b5edf9ac54f10e9
1linyan <1>
2023-07-20 09:17:56 +0800

    新增侧滑功能

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createSSRApp } from "vue";
 import App from "./App.vue";
 import "./static/css/them.scss"
 import uviewPlus from "uview-plus";
+// npm i --save-dev @types/uview-plus
 import messages from './locale/index'
 import { createI18n  } from 'vue-i18n'
 import { Pinia } from "./plugins/pinia/pinia";
