4f381ea7e10c8c0fee07a1c53a619467d8afa00c
1linyan <1>
2023-09-19 17:24:26 +0800

    旋转360选车

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,22 +1,20 @@
 <template>
-	 <web-view src="http://8.217.125.245/h5#/"></web-view>
+	<view class="centers">
+		 1
+	</view>
 </template>
 
 
 <script setup lang="ts">
-import { ref } from "vue"; 
+	import { ref } from "vue";
+	 
+	 
+	 
 </script>
 
 <style scoped lang="scss">
-@import url("../../static/css/animate.css");
-@import "./css/login.scss";
-.ssss{
-	padding-top: 20%;
-}
-.anim{
+	@import url("../../static/css/animate.css");
+	@import "./css/login.scss";
+
 	 
-	animation-duration: 2s;//动画效果时间 
-	// animation-delay: 2s;//动画延迟加载时间
-	animation-iteration-count: infinite;//动画是否循环显示
-}
 </style>
