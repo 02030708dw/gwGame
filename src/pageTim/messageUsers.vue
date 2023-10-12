81d22095ba60c059b8b5a9249b03ac5c4b491857
1linyan <1>
2023-10-12 09:00:04 +0800

    聊天

--- src/pageTim/messageUsers.vue
+++ src/pageTim/messageUsers.vue
@@ -1,30 +1,27 @@
 <template>
 	<scroll-view :scroll-y="true" :enable-flex="true" class="chat-style">
 
-		<view class="chat-style-list" @click="handleSendMessage(item)" v-for="(item,index) in TIMStore.conversationList" :key="index">
-			 <view class="avers">
-			 	
-			 </view> 
-			 <view class="aversTitle">
-			 	 <view class="aversTitles textEllipsis">{{item.lastMessage.fromAccount}} </view>
-			 	 <view class="aversTitles2 textEllipsis">{{item.lastMessage.payload.text}} </view>
-			 </view>
+		<view class="chat-style-list" @click="handleSendMessage(item)" v-for="(item,index) in TIMStore.conversationList"
+			:key="index">
+			<view class="avers">
+
+			</view>
+			<view class="aversTitle">
+				<view class="aversTitles textEllipsis">{{item.lastMessage.fromAccount}} </view>
+				<view class="aversTitles2 textEllipsis">{{item.lastMessage.payload.text}} </view>
+			</view>
 		</view>
 
 	</scroll-view>
 </template>
 
 <script lang="ts" setup>
-	import { ref, computed } from "vue";
-	import uvChatInput from "@/components/chat/uv-chat-input/uv-chat-input.vue";
-
 	import { useTIMStore } from "@/plugins/chat"
 	import { useSendGetUser } from "@/plugins/TIM-plugin/sendGetUser.pinia"
 
 	const storeSendGetUser = useSendGetUser();
 	const TIMStore = useTIMStore();
-	const payloadText = ref('')
- console.log(TIMStore.conversationList,'TIMStore.conversationList')
+	console.log(TIMStore.conversationList, 'TIMStore.conversationList')
 
 
 	TIMStore.timCore.onReady = async () => {
@@ -40,15 +37,12 @@
 	const handleSendMessage = async (item : any) => {
 		console.log('设置像谁发送消息', item.lastMessage.fromAccount)
 		storeSendGetUser.sendUser = item.lastMessage.fromAccount
-       uni.navigateTo({
-       	url:'/pageTim/message'
-       })
-		// 获取对应人（冉先生）的历史记录
-		// await TIMStore.getMessageHistoryList(storeSendGetUser.sendUser)
+		uni.navigateTo({
+			url: '/pageTim/message'
+		})
 	}
 </script>
 
 <style lang="scss" scoped>
 	@import "./css/messageUsers.scss";
-	 
 </style>
