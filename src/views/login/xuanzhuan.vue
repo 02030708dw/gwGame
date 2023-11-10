4d6964111c92907827af187179728942b1ccaa33
ranjingqiao <3051813192@qq.com>
2023-11-10 21:48:02 +0800

    1

---
+++ src/views/login/xuanzhuan.vue
@@ -0,0 +1,15 @@
+<template>
+	<view class="">
+		 <xuanzhuan/>
+	</view>
+</template>
+
+
+<script setup lang="ts"> 
+	import xuanzhuan from "@/components/xuanzhuan/index"
+	 
+</script>
+
+<style scoped lang="scss">
+ 
+</style>
