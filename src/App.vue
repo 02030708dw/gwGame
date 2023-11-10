3cff24dce1c6d23b0c9aedbfec4ddb0c4aab19bd
1linyan <1>
2023-11-10 16:57:29 +0800

    旋转

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   @Author: ranjingqiao
  -->
 <script setup lang="ts">
+	 
 	import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
 	onLaunch(() => {
 		console.log("App Launch");
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,5 +1,27 @@
 <template>
-	<view class="">
+	<xuanzhuan/>
+	
+	
+	
+	01 03 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 D0 E8 
+	
+	
+	
+	00 03 02 00 00 05 BA 00 77 0A 3C 00 00 00 00 00 00 00 98 46
+	
+	01--->执行中
+	02--->执行完毕
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	<!-- <view class="">
 		<view class="bacAlipay">
 
 			<u-form ref="uForm">
@@ -15,12 +37,13 @@
 			<view @click="loginOut" class="submitAlipay">退出登录</view>
 		</view>
 
-	</view>
+	</view> -->
 </template>
 
 
 <script setup lang="ts">
 	import { ref } from "vue"; 
+	import xuanzhuan from "@/components/xuanzhuan/index.vue"
 	import { genTestUserSig } from "@/debug/index.js"
 	import { useTIMStore } from "@/plugins/chat"
 	import { configTim } from "@/plugins/TIM-plugin/configTim"
