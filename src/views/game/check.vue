07e2aef517ab618ca826e826c600d2dc3e8531b9
chenaoyv <chen.aoyv@outlook.com>
2023-12-19 16:32:04 +0800

    账变记录结构完成

--- src/views/game/check.vue
+++ src/views/game/check.vue
@@ -3,10 +3,19 @@
     <gameHeader :showContent="!showHeaderContent" activeTitle="账变纪录" />
     <SearchDatePicker @selectedDates="onSelectedDates" />
 
-    <view class="bill"> </view>
+    <view class="bill" v-for="item in 10">
+      <text class="top">2000/10/02 01:10</text>
+      <view class="center">
+        <text> [+] 奖金派送</text>
+        <text>+3.2</text>
+      </view>
+      <view class="bottom">
+        <text>可用余额</text>
+        <text>4,999,020.2</text>
+      </view>
+    </view>
   </view>
 </template>
-
 <script setup lang="ts">
 import gameHeader from "@/components/game/gameHeader.vue";
 import SearchDatePicker from "@/components/SearchDatePicker.vue";
@@ -15,13 +24,6 @@ const showHeaderContent = ref(true);
 const onSelectedDates = (dates: string) => {
   console.log(dates);
 };
-const items = ref([
-  { value: "4", position: "left", color: "1", name: "2", type: "3" },
-  { value: "4", position: "left", color: "1", name: "2", type: "3" },
-  { value: "4", position: "left", color: "1", name: "2", type: "3" },
-  { value: "4", position: "left", color: "1", name: "2", type: "3" },
-  { value: "4", position: "left", color: "1", name: "2", type: "3" },
-]);
 </script>
 <style scoped lang="scss">
 .billing-details {
@@ -30,10 +32,25 @@ const items = ref([
     height: 204rpx;
     background: #ffffff;
     border-radius: 24rpx;
-    margin:20rpx auto 0;
+    margin: 20rpx auto 0;
     box-sizing: border-box;
-    border:  1px solid #000;
     padding: 32rpx;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .top {
+      color: #c1c1c1;
+    }
+    .center {
+      font-size: 36rpx;
+      display: flex;
+      justify-content: space-between
+    }
+    .bottom {
+      color: #c1c1c1;
+      display: flex;
+      justify-content: space-between
+    }
   }
 }
 </style>
