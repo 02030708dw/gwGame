92c64846b9847a8a64f6263f447032affc34d69f
chenaoyv <chen.aoyv@outlook.com>
2023-12-25 17:42:19 +0800

    已选号码传入弹出层

---
+++ src/components/game/YN/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <view class="footer">
+    <image
+      src="@/static/images/footerleft.png"
+      style="width: 64rpx; height: 52rpx;margin-right: 20rpx;"
+    />
+    <view class="titleLeft">27,973.00</view>
+    <u-icon name="arrow-right" color="#999" size="16"></u-icon>
+    <image
+      src="@/static/images/footerq1.png"
+      style="width: 64rpx; height: 64rpx; margin-left: 100rpx"
+    />
+    <image
+      src="@/static/images/footerq2.png"
+      style="width: 64rpx; height: 64rpx; margin-left: 16rpx"
+    />
+    <view class="btn" @click="clickHandle">投注</view>
+  </view>
+</template>
+<script setup lang="ts">
+const emits=defineEmits(['clickHandle'])
+const clickHandle=()=>emits('clickHandle')
+</script>
+<style lang="scss">
+.footer {
+  background-color: #fff;
+  position: fixed;
+  width: 750rpx;
+  height: 96rpx;
+  bottom: 0;
+  box-sizing: border-box;
+  padding: 0 32rpx;
+  display: flex;
+  align-items: center;
+  .btn {
+    width: 152rpx;
+    height: 72rpx;
+    background: #ffb023;
+    border-radius: 8rpx;
+    font-size: 44rpx;
+    font-weight: 600;
+    color: #ffffff;
+    line-height: 72rpx;
+    text-align: center;
+    margin-left: 32rpx;
+  }
+}
+</style>
--- src/views/game/vietnameseLottery.vue
+++ src/views/game/vietnameseLottery.vue
@@ -6,68 +6,68 @@
     <GameHeaderTab :typeTab="typeTab" />
     <GameContent />
     <GameTime />
-
     <!-- 选择2D,3D,PL2,PL3 -->
     <GameType @cutGameType="cutGameType" :typeList="typeList" />
 
     <!-- 2D----------------------------------- -->
-        <SelectMethed
-          :background-image="urls1"
-          :row="3"
-          @change="changeSelectMethed"
-          v-show="playingMethod == 0" 
-          />
-        <KeyNum
-          :background-image="urls1"
-          :showHeader="false"
-          @changeNum="changeNum2D"
-          v-show="playingMethod == 0"
-          :unlock="twoD.length"
-      />
+    <SelectMethed
+      :background-image="urls1"
+      :row="3"
+      @change="changeSelectMethed"
+      v-show="playingMethod == 0"
+    />
+    <KeyNum
+      :background-image="urls1"
+      :showHeader="false"
+      @changeNum="changeNum2D"
+      v-show="playingMethod == 0"
+      :unlock="twoD.length"
+    />
 
     <!-- 3D------------------------------------- -->
-        <SelectMethed
-          :background-image="urls1"
-          :row="3"
-          @change="changeSelectMethed"
-          v-show="playingMethod == 1"
-        />
-        <KeyNum
-          :background-image="urls1"
-          :showHeader="true"
-          @changeNum="changeNum3D"
-          v-show="playingMethod == 1"
-          :unlock="threeD.length"
-        />
+    <SelectMethed
+      :background-image="urls1"
+      :row="3"
+      @change="changeSelectMethed"
+      v-show="playingMethod == 1"
+    />
+    <KeyNum
+      :background-image="urls1"
+      :showHeader="true"
+      @changeNum="changeNum3D"
+      v-show="playingMethod == 1"
+      :unlock="threeD.length"
+    />
 
     <!-- PL2--------------------------------------- -->
-        <KeyNum
-          :background-image="urls1"
-          :showHeader="false"
-          :astrict="2"
-          @changeNum="changeNumPL2"
-          v-show="playingMethod == 2"
-          :unlock="true"
-        />
-
+    <KeyNum
+      :background-image="urls1"
+      :showHeader="false"
+      :astrict="2"
+      @changeNum="changeNumPL2"
+      v-show="playingMethod == 2"
+      :unlock="true"
+    />
 
     <!-- PL3 ------------------------------------------->
-        <KeyNum
-          :background-image="urls1"
-          :showHeader="false"
-          :astrict="3"
-          @changeNum="changeNumPL3"
-          v-show="playingMethod == 3"
-          :unlock="true"
-        />
+    <KeyNum
+      :background-image="urls1"
+      :showHeader="false"
+      :astrict="3"
+      @changeNum="changeNumPL3"
+      v-show="playingMethod == 3"
+      :unlock="true"
+    />
     <template #bot>
-      <GameFooter />
+      <!-- <GameFooter /> -->
+      <Footer @click-handle="clickBet" />
     </template>
   </Layout>
   <popup />
+  <BetListPop :show="show" @close="closeBetList" :list="betlist" @del="delBetList"/>
 </template>
 <script lang="ts" setup>
-import { reactive, ref, toRefs } from "vue";
+import { reactive, ref, toRefs, computed } from "vue";
 import { storeToRefs } from "pinia";
 import GameHeader from "@/components/game/gameHeader.vue";
 import GameTime from "@/components/game/gameTime.vue";
@@ -75,11 +75,12 @@ import Layout from "@/layout/index.vue";
 import GameHeaderTab from "@/components/game/gameHeaderTab.vue";
 import GameContent from "@/components/game/gameContent.vue";
 import GameFooter from "@/components/game/gameFooter.vue";
+import Footer from "@/components/game/YN/Footer.vue";
 import popup from "@/components/game/popup/popup.vue";
-
 import GameType from "@/components/game/YN/GameType.vue";
 import SelectMethed from "@/components/game/YN/SelectMethed.vue";
 import KeyNum from "@/components/game/YN/KeyNum.vue";
+import BetListPop from "@/components/game/YN/BetListPop.vue";
 import { usethreeMinute } from "@/plugins/pinia/YNthreeMinute";
 const storethreeMinute = usethreeMinute();
 const { typeList, methodList } = storeToRefs(storethreeMinute);
@@ -92,41 +93,86 @@ const typeTab = reactive([
 let urls1 = ref("src/static/images/fredHill1.png");
 let urls2 = ref("src/static/images/fredHill2.png");
 let urls3 = ref("src/static/images/fredHill3M.png");
-
 const playingMethod = ref(0); //用来展示不同玩法
-
 // 类型切换------------------------------------
 const cutGameType = (item: any) => {
-  // 每次切换类型时,取消游戏玩法的选中
-  // methodList.value.forEach((item) => (item.checked = false));
   playingMethod.value = item.id;
 };
+const betlist: any = ref([]); //全部选中的玩法
 
 const twoD = ref([]); //2d选择的玩法
 const threeD = ref([]); //2d选择的玩法
-// 玩法选中
+
+// 玩法选中-----------------------------------
 const changeSelectMethed = (selectData: any) => {
   // 判断当前是2d,还是3d
-  if (playingMethod.value == 0) {
-    twoD.value = selectData;
+  if (playingMethod.value == 0) twoD.value = selectData;
+  if (playingMethod.value == 1) threeD.value = selectData;
+};
+// 2D--------------------------------------------
+const num2D = ref([]); //2d所选的全部数字
+const active2D = computed(() => {
+  //计算2d里面选中的玩法与数字
+  return twoD.value.map((item: any) => {
+    return { label: "2D-" + item.label, num: [...num2D.value],sum:item.sum,id:crypto.randomUUID() };
+  });
+});
+const changeNum2D = (selectNumber: any) => {
+  let num = selectNumber.map((item: any) => item.label); //选中的号码
+  num2D.value = num;
+};
+// 3D--------------------------------------------------------
+const num3D = ref([]); //3D所选的全部数据
+const active3D = computed(() => {
+  return threeD.value.map((item: any) => {
+    return { label: "3D-" + item.label, num: [...num3D.value] ,sum:item.sum,id:crypto.randomUUID() };
+  });
+});
+const changeNum3D = (selectNumber: any) => {
+  let num = selectNumber.map((item: any) => item.label); //选中的号码
+  num3D.value = num;
+  console.log(num)
+};
+// PL2---------------------------
+const numPL2 = ref([]);
+const activePL2 = computed(() => {
+  if(numPL2.value.length==2){
+    return { label: "PL2" , num: [...numPL2.value],sum:36 ,id:crypto.randomUUID()};
   }
-  if (playingMethod.value == 1) {
-    threeD.value = selectData;
-  }
+});
+const changeNumPL2 = (selectNumber: any) => {
+  let num = selectNumber.map((item: any) => item.label); //选中的号码
+  numPL2.value = num;
 };
 
-const changeNum2D = (selectNumber: any) => {
-  console.log(twoD.value);
-  console.log(selectNumber);
-};
-const changeNum3D = (selectNumber: any) => {
-  console.log(threeD.value);
-  console.log(selectNumber);
-};
-const changeNumPL2 = (selectNumber: any) => {
-  console.log(selectNumber);
-};
+// PL3---------------------------
+const numPL3 = ref([]);
+const activePL3 = computed(() => {
+  if(numPL3.value.length==3){
+    return { label: "PL3" , num: [...numPL3.value],sum:54 ,id:crypto.randomUUID()};
+  }
+});
 const changeNumPL3 = (selectNumber: any) => {
-  console.log(selectNumber);
+  let num = selectNumber.map((item: any) => item.label); //选中的号码
+  numPL3.value = num;
+};
+
+// 删除一项
+const delBetList=(id:string)=>{
+  console.log(id)
+  betlist.value=betlist.value.filter((item:any)=>item.id!==id)
+}
+const show = ref(false);
+const clickBet = () => {
+  // 打开底部弹出层,同时要将选中的号码传入
+  show.value = true;
+  let arr=[...active2D.value, ...active3D.value,activePL2.value,activePL3.value];
+  betlist.value = arr.filter(item=>item)
+  console.log(betlist.value)
+};
+
+const closeBetList = () => {
+  show.value = false;
+  // 关闭底部弹出层
 };
 </script>
