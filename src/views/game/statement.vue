56c0fa0bba6e5d176470c7797e7db3c70311808b
DK <2632454453@qq.com>
2023-12-26 17:24:07 +0800

    帐变记录,盈亏报表api

--- src/views/game/statement.vue
+++ src/views/game/statement.vue
@@ -6,57 +6,91 @@
       <text>盈亏查询</text>
       <text>{{ total }}</text>
     </view>
-    <view class="profit-item" v-for="item in timeItems"> 
-      <text>{{ item.title }}</text>
-      <text>{{ item.value }}</text>
+    <view class="profit-items">
+      <view class="profit-item" v-for="item in timeItems" :key="item.day">
+        <text>{{ item.day }}</text>
+        <text>{{ item.profitAmount }}</text>
+      </view>
     </view>
   </view>
 </template>
 <script setup lang="ts">
 import gameHeader from "@/components/game/gameHeader.vue";
 import SearchDatePicker from "@/components/SearchDatePicker.vue";
-import { ref, computed } from "vue";
+import { ref, computed, onBeforeMount } from "vue";
+import { get } from "@/api";
+
 const showHeaderContent = ref(true);
-const timeItems = ref([
-  { title: "2000/3/15", value: 0.8 },
-  { title: "2000/3/15", value: 3.2 },
-  { title: "2000/3/15", value: -3.7 },
-  { title: "2000/3/15", value: 4.7 },
-]);
+interface TimeItem {
+  day: string;
+  betAmount: number;
+  awardAmount: number;
+  profitAmount: number;
+}
+const timeItems = ref([]);
 const total = computed(() => {
-  return timeItems.value.reduce((total, item) => total + item.value, 0);
+  return timeItems.value.reduce((total, item) => total + item.profitAmount, 0);
 });
 const onSelectedDates = (dates: string) => {
   console.log(dates);
 };
+
+const fetchData = async () => {
+  try {
+    const res = await get({ url: "/gameRecords/member/statistics" });
+    timeItems.value = res.resultSet.map((item: TimeItem) => ({
+      ...item,
+      day: formatDate(item.day),
+    }));
+  } catch (err) {
+    console.error("Error fetching data:", err);
+  }
+};
+
+const formatDate = (dateString: string) => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  };
+  return new Intl.DateTimeFormat("zh-CN", options).format(new Date(dateString));
+};
+
+onBeforeMount(() => {
+  fetchData();
+});
 </script>
 <style scoped lang="scss">
-body{
-background-color: #F9F9F9;
-.profit {
-  background-color: #F9F9F9;
-  .synthesize {
-    width: 622rpx;
-    box-sizing: border-box;
-    margin: 32rpx auto 0;
-    color: #bcbcbc;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+body {
+  background-color: #f9f9f9;
+  .profit {
+    background-color: #f9f9f9;
+    .synthesize {
+      width: 622rpx;
+      box-sizing: border-box;
+      margin: 32rpx auto 0;
+      color: #bcbcbc;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    .profit-item {
+      width: 686rpx;
+      height: 100rpx;
+      background: #ffffff;
+      border-radius: 24rpx;
+      box-sizing: border-box;
+      margin: 20rpx auto 0;
+      padding: 32rpx;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      font-size: 36rpx;
+    }
   }
-  .profit-item {
-    width: 686rpx;
-    height: 100rpx;
-    background: #ffffff;
-    border-radius: 24rpx;
-    box-sizing: border-box;
-    margin: 20rpx auto 0;
-    padding: 32rpx;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    font-size: 36rpx;
-  }
-}
 }
 </style>
