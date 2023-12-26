7728d0e15bb46f8e72adc9747aed6ef9633c7d19
DK <2632454453@qq.com>
2023-12-26 09:40:52 +0800

    动画

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,6 +2,7 @@
 
 export const IMAGE_URL = '' // 静态资源的地址
 
+// export const HTTP_REQUEST_URL = 'http://192.168.124.15:7001'
 export const HTTP_REQUEST_URL = 'http://192.168.124.3:7001'
 
 export const HEADER = {
