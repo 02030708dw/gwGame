177d645a629c0036593821475f835de4e257b889
小丑在唱独角戏 <10097251+xczcdjx@user.noreply.gitee.com>
2024-01-08 18:42:57 +0800

    倒计时修改2

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,24 +3,23 @@ import {ref, Ref} from "vue";
 
 export default function (url: string, delay = 1000, distance: number = 5) {
     const abort = ref<boolean>(false)
-    const getGameAward = (data: any, callback: (...args: any) => void) => new Promise((resolve,reject) =>
-        post2({url, data}).then(v => {
-            const time=Number(v.resultSet.awardNum.countdown)
+    const timer=ref(123)
+    const getGameAward = (data: any, callback: (...args: any) => void) => new Promise((resolve,reject) => {
+            post2({url, data}).then(v => {
+                timer.value = Number(v.resultSet.awardNum.countdown)
                 if (abort.value) {
                     reject('abortion');
                     return;
                 }
-                console.log(time,time-distance)
-                if (time-distance <= 0) {
+                if (timer.value - distance <= 0) {
                     callback(v)
-                    console.log(v)
                     setTimeout(() => getGameAward(data, callback), delay)
-                }else {
-                    console.log(v)
+                } else {
                     resolve(v)
                     return;
                 }
             })
+        }
     )
     return {
         getGameAward, abort
